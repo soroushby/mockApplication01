4f1ba7547da9f19e5007b1bd421606094f8889b8
soroushby <49848393+soroushby@users.noreply.github.com>
2020-03-23 23:53:05 -0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mockApp01';
+
+  displaymessage (){
+    console.log('this message has been displayed')
+  }
+  
+ movies = ["kill bill","inception","fight club"];
+
+
+
+ pantea (tool,arz) {
+let surface = tool*arz
+
+return surface;
+
+}
+
 }
